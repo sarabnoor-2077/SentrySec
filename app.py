fc1c6db6ef0b9cfa2e49f171e5b4cdc52b69f368
sarabnoor-2077 <sarabnoor2077@gmail.com>
2026-08-04 02:43:23 +0530

    Added Flask interface and file upload

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import os
+
+from analyser.parser import read_log_file, parse_log_entry
+from analyser.detector import (
+    detect_brute_force,
+    detect_success_after_failures
+)
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    file = request.files["logfile"]
+
+    filepath = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+
+    file.save(filepath)
+
+    logs = read_log_file(filepath)
+
+    parsed_logs = [parse_log_entry(log) for log in logs]
+
+    brute_force = detect_brute_force(parsed_logs)
+
+    compromise = detect_success_after_failures(parsed_logs)
+
+    return render_template(
+        "results.html",
+        brute_force=brute_force,
+        compromise=compromise
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
